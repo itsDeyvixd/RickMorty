99e49ed3b8b692817cedac959b17f83d06b24099
Deyvi <dfrioman@gmail.com>
2024-10-05 11:38:51 -0500

    Navbar Fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = ({ brand }) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container justify-content-center">
-                <a className="navbar-brand" href="/">
+                <a className="navbar-brand" href="#home">
                 <img src={logo} alt="Logo" className="navbar-brand-image"width="40" height="40" /> 
                 <Navbar1.Brand href="#home" style={{marginLeft:"20px"}} className="rickFont">Rick and morty app By Deyvi</Navbar1.Brand>                
                 </a>
